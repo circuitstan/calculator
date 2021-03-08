1613fe71f3be78316f364b222b2c064f41b8fba5
Stenver Suurkütt <stenver.ss@gmail.com>
2021-03-08 16:52:47 +0200

    added multiple calculations functionality, decimal places, rounding of large numbers and error message when attempting to divide by 0

--- script.js
+++ script.js
@@ -27,29 +27,100 @@ const operators = document.querySelectorAll('button.operators')
 let a = ""
 let b = ""
 let operator = ""
+let saveCalc = []
 
 function display() {
     
     numbers.forEach((button) => {
         
         button.addEventListener('click', (e) => {
+            if (displayed.textContent == "ERROR") {
+                cleardisplay()
+            }
             if (displayed.textContent == "0") {
                 displayed.textContent = ""
             }
-            if (displayed.textContent.includes(" ")) {
+            if (saveCalc.length > 1) {
+                if (displayed.textContent.includes(".")) {
+                    if (e.target.innerText == ".") {
+                        return
+                    } else {
+                        b += e.target.innerText
+                        displayed.textContent = b
+                        return
+                    }
+                }
                 b += e.target.innerText
+                displayed.textContent = b
             } else {
+                if (displayed.textContent.includes(".")) {
+                    if (e.target.innerText == ".") {
+                        return
+                    } else {
+                        a += e.target.innerText
+                        displayed.textContent = a
+                        return
+                    }
+                }
                 a += e.target.innerText
+                displayed.textContent = a
             }
-            displayed.textContent = a + operator + b
                 
         })
     })
     operators.forEach((button) => {
         
         button.addEventListener('click', (e) => {
-            operator = " " + e.target.innerText + " "
-            displayed.textContent = a + operator + b
+            console.log(saveCalc.length)
+            if (displayed.textContent == "ERROR") {
+                cleardisplay()
+            }
+            if (saveCalc.length >= 1) {
+                if (b == "") {
+                    return
+                }
+                saveCalc[2] = b
+                console.log(saveCalc)
+                a = Number(saveCalc[0])
+                b = Number(saveCalc[2])
+                if (b == 0) {
+                    displayed.textContent = "ERROR"
+                    return
+                }
+                if (saveCalc[1] == "+") {
+                    displayed.textContent = operate(add, a, b)
+                    displayed.textContent = decimals(displayed.textContent)
+                    saveCalc[1] = ""
+                    saveCalc[0] = displayed.textContent
+                    a = ""
+                    b = ""
+                } else if (saveCalc[1] == "-") {
+                    displayed.textContent = operate(subtract, a, b)
+                    displayed.textContent = decimals(displayed.textContent)
+                    saveCalc[1] = ""
+                    saveCalc[0] = displayed.textContent
+                    a = ""
+                    b = ""
+                } else if (saveCalc[1] == "x") {
+                    displayed.textContent = operate(multiply, a, b)
+                    displayed.textContent = decimals(displayed.textContent)
+                    saveCalc[0] = displayed.textContent
+                    a = ""
+                    b = ""
+                } else if (saveCalc[1] == "÷") {
+                    displayed.textContent = operate(divide, a, b)
+                    displayed.textContent = decimals(displayed.textContent)
+                    saveCalc[0] = displayed.textContent
+                    a = ""
+                    b = ""
+                }
+                saveCalc[1] = e.target.innerText
+            } else {
+                saveCalc[0] = displayed.textContent
+                operator = e.target.innerText
+                saveCalc[1] = operator
+                console.log(saveCalc)
+            }
         })
     })
 }
@@ -60,30 +131,62 @@ const clear = document.querySelector('#clear')
 
 function calculate() {
     
-    equals.addEventListener('click', (e) => {
-        let ans = displayed.textContent.split(" ")
-        a = parseInt(ans[0])
-        b = parseInt(ans[2])
-        if (ans[1] == "+") {
-            displayed.textContent = operate(add, a, b)
-        } else if (ans[1] == "-") {
-            displayed.textContent = operate(subtract, a, b)
-        } else if (ans[1] == "x") {
-            displayed.textContent = operate(multiply, a, b)
-        } else if (ans[1] == "÷") {
-            displayed.textContent = operate(divide, a, b)
+    equals.addEventListener('click', (e) => {        
+        if (displayed.textContent == "ERROR") {
+            cleardisplay()
         }
-
+        if (saveCalc.length < 1) {
+            return
+        }
+        if (b == "") {
+            return
+        }
+        if (b == 0) {
+            displayed.textContent = "ERROR"
+            return
+        }
+        console.log(saveCalc.length)
+        a = Number(saveCalc[0])
+        console.log(a)
+        saveCalc[2] = displayed.textContent
+        b = Number(saveCalc[2])
+        if (saveCalc[1] == "+") {
+            displayed.textContent = operate(add, a, b)
+            displayed.textContent = decimals(displayed.textContent)
+        } else if (saveCalc[1] == "-") {
+            displayed.textContent = operate(subtract, a, b)
+            displayed.textContent = decimals(displayed.textContent)
+        } else if (saveCalc[1] == "x") {
+            displayed.textContent = operate(multiply, a, b)
+            displayed.textContent = decimals(displayed.textContent)
+        } else if (saveCalc[1] == "÷") {
+            displayed.textContent = operate(divide, a, b)
+            displayed.textContent = decimals(displayed.textContent)
+        }
     })
 }
 calculate()
 
 function cleardisplay() {
-    clear.addEventListener('click', () => {
-        displayed.textContent = 0
-        a = ""
-        b = ""
-        operator = ""
-    })
+    displayed.textContent = 0
+    a = ""
+    b = ""
+    operator = ""
+    saveCalc = []
+    }
+
+clear.addEventListener('click', () => {
+    cleardisplay()
+})
+
+function decimals(int) {
+    if (int.includes(".") == true) {
+        let ans = int.split(".")
+        if ((ans[0].length + ans[1].length) > 12) {
+            int = Number(int)
+            int = int.toPrecision(12)
+            int = parseFloat(int)
+            return int
+        } else return int
+    } else return int
 }
-cleardisplay()
